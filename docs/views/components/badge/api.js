9b1aefe5ae799f791d32cc26e3e557054eb51342
Vegar Eeg <vegareeg@gmail.com>
2018-11-11 20:41:37 +0100

    Update badge and select api definitions

--- docs/views/components/badge/api.js
+++ docs/views/components/badge/api.js
@@ -3,94 +3,59 @@ export default [
     props: [
       {
         name: 'placeholder',
-        type: '<i>string</i>',
+        type: '<i>String</i>',
         values: '—',
-        default: '<code class="tag">undefined</code>',
+        default: '<u>undefined</u>',
         description: 'Default text'
       },
       {
         name: 'multiple',
-        type: '<i>boolean</i>',
+        type: '<i>Boolean</i>',
         values: '—',
-        default: '<code class="tag">false</code>',
+        default: '<u>false</u>',
         description: 'Enables selection of multiple elements'
       },
       {
         name: 'disabled',
-        type: '<i>boolean</i>',
+        type: '<i>Boolean</i>',
         values: '—',
-        default: '<code class="tag">false</code>',
-        description: 'Disables the reference element'
+        default: '<u>false</u>',
+        description: 'Disables the reference element. Lorem ipsum dolor sit amt.'
       },
       {
         name: 'hide-label',
-        type: '<i>boolean</i>',
+        type: '<i>String, Boolean</i>',
         values: '—',
-        default: '<code class="tag">false</code>',
+        default: '<u>false</u>',
         description: 'Hides the label'
       },
       {
         name: 'hide-label-focused',
-        type: '<i>boolean</i>',
+        type: '<i>Boolean</i>',
         values: '—',
-        default: '<code class="tag">false</code>',
+        default: '<u>false</u>',
         description: 'Hides the label only when focused'
       },
       {
         name: 'transition',
-        type: '<i>string</i>',
-        values: '<code class="tag">slide-down</code> <code class="tag">scale-top-left</code> <code class="tag">fade</code>',
-        default: '<code class="tag">slide-down</code>',
+        type: '<i>String',
+        values: '<code class="tag">slide-downscale</code> <code class="tag">top-left</code> <code class="tag">fade</code>',
+        default: '<u>slide-down</u>',
         description: 'Specifies the dropdown animation'
-      },
-      {
-        name: 'auto-width',
-        type: '<i>boolean</i>',
-        values: '—',
-        default: '<code class="tag">false</code>',
-        description: 'Dropddown fits inner content'
-      },
-      {
-        name: 'show-checkbox',
-        type: '<i>boolean</i>',
-        values: '—',
-        default: '<code class="tag">false</code>',
-        description: 'Display a checkbox in each row'
-      },
-      {
-        name: 'value',
-        type: '<i>string</i> <i>array</i>',
-        values: '—',
-        default: '<code class="tag">undefined</code>',
-        description: 'Currently selcted item(s)'
-      },
-      {
-        name: 'options',
-        type: '<i>array</i>',
-        values: '—',
-        default: '<code class="tag">undefined</code>',
-        description: 'Dropdown items'
-      },
-      {
-        name: 'theme',
-        type: '<i>string</i>',
-        values: '<code class="tag">material</code> <code class="tag">boxed</code>',
-        default: '<code class="tag">material</code>',
-        description: 'Specifies reference element look and feel'
       }
     ],
     events: [
       {
         name: 'input',
-        description: 'Triggers when an item has been selected/deselected',
-        Value: '<code class="tag">String</code> <code class="tag">Array</code>'
+        value: 'String | Array',
+        description: 'Triggers when an item has been selected/deselected'
       }
     ],
     methods: [
       {
         name: 'trubadur',
-        description: 'Lorem ipsum dolor sit amet',
-        Value: '<i>string</i>'
+        value: 'String',
+        description: 'Lorem ipsum dolor sit amet'
       }
     ]
   }
--- docs/views/components/select/api.js
+++ docs/views/components/select/api.js
@@ -5,77 +5,77 @@ export default [
         name: 'placeholder',
         type: '<i>string</i>',
         values: '—',
-        default: '<code class="tag">undefined</code>',
+        default: '<u>undefined</u>',
         description: 'Default text'
       },
       {
         name: 'multiple',
         type: '<i>boolean</i>',
         values: '—',
-        default: '<code class="tag">false</code>',
+        default: '<u>false</u>',
         description: 'Enables selection of multiple elements'
       },
       {
         name: 'disabled',
         type: '<i>boolean</i>',
         values: '—',
-        default: '<code class="tag">false</code>',
+        default: '<u>false</u>',
         description: 'Disables the reference element'
       },
       {
         name: 'hide-label',
         type: '<i>boolean</i>',
         values: '—',
-        default: '<code class="tag">false</code>',
+        default: '<u>false</u>',
         description: 'Hides the label'
       },
       {
         name: 'hide-label-focused',
         type: '<i>boolean</i>',
         values: '—',
-        default: '<code class="tag">false</code>',
+        default: '<u>false</u>',
         description: 'Hides the label only when focused'
       },
       {
         name: 'transition',
         type: '<i>string</i>',
         values: '<code class="tag">slide-down</code> <code class="tag">scale-top-left</code> <code class="tag">fade</code>',
-        default: '<code class="tag">slide-down</code>',
+        default: '<u>slide-down</u>',
         description: 'Specifies the dropdown animation'
       },
       {
         name: 'auto-width',
         type: '<i>boolean</i>',
         values: '—',
-        default: '<code class="tag">false</code>',
+        default: '<u>false</u>',
         description: 'Dropddown fits inner content'
       },
       {
         name: 'show-checkbox',
         type: '<i>boolean</i>',
         values: '—',
-        default: '<code class="tag">false</code>',
+        default: '<u>false</u>',
         description: 'Display a checkbox in each row'
       },
       {
         name: 'value',
         type: '<i>string</i> <i>array</i>',
         values: '—',
-        default: '<code class="tag">undefined</code>',
+        default: '<u>undefined</u>',
         description: 'Currently selcted item(s)'
       },
       {
         name: 'options',
         type: '<i>array</i>',
         values: '—',
-        default: '<code class="tag">undefined</code>',
+        default: '<u>undefined</u>',
         description: 'Dropdown items'
       },
       {
         name: 'theme',
         type: '<i>string</i>',
         values: '<code class="tag">material</code> <code class="tag">boxed</code>',
-        default: '<code class="tag">material</code>',
+        default: '<u>material</u>',
         description: 'Specifies reference element look and feel'
       }
     ],
