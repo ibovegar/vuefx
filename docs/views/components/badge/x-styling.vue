56e1782367d9002af0ebafda9852bf6763b41f7a
Vegar Eeg <vegareeg@gmail.com>
2018-11-12 23:47:28 +0100

    Add usage section to badge documentation

--- docs/views/components/badge/x-styling.vue
+++ docs/views/components/badge/x-styling.vue
@@ -3,13 +3,26 @@
     <fx-badge :value="12">
       Default color
     </fx-badge>
-    <fx-badge :value="456" fill="primary">
+    <fx-badge
+      :value="456"
+      fx-color="primary"
+      >
       Custom color
     </fx-badge>
-    <fx-badge :value="5" fill="fill-1" border="alert" font="alert">
+    <fx-badge
+      :value="5"
+      fx-color="fill-1"
+      fx-border="alert"
+      fx-font="alert"
+      >
       Outline
     </fx-badge>
-    <fx-badge :value="7" fill="fill-1" font="font-1" raised>
+    <fx-badge
+      :value="7"
+      fx-color="fill-1"
+      fx-font="font-1"
+      raised
+      >
       Raised
     </fx-badge>
   </section>
