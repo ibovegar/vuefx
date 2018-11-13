5a34028d295b0b3ec6ab8fc026b53e4a9c972d93
Vegar Eeg <vegareeg@gmail.com>
2018-11-13 23:11:03 +0100

    Implement wrapping

--- src/components/Chip/ChipGroup.vue
+++ src/components/Chip/ChipGroup.vue
@@ -1,6 +1,8 @@
 <template>
   <div
     class="fx-chip-group"
+    :class="rootClasses"
+    :style="colorStyles"
     v-drag-scroll
     >
     <slot/>
@@ -9,11 +11,23 @@
 
 <script>
 import DragScroll from '@/directives/drag-scroll'
+import ColorMixin from '@/mixins/colors.js'
 
 export default {
   name: 'fx-chip-group',
 
+  mixins: [ ColorMixin ],
+
   directives: { DragScroll },
+
+  props: {
+    scrollable: Boolean,
+    align: {
+      type: String,
+      default: 'left',
+      validator: value => { return ['left', 'right', 'center'].includes(value) }
+    }
+  },
 
   updated () {
     this.checkOverflow()
@@ -27,6 +41,17 @@ export default {
     }
   },
 
+  computed: {
+    rootClasses: function () {
+      return {
+        'is-left': this.align === 'left',
+        'is-right': this.align === 'right',
+        'is-center': this.align === 'center',
+        'is-scrollable': this.scrollable
+      }
+    }
+  },
+
   mounted () {
     this.checkOverflow()
   }
@@ -35,16 +60,41 @@ export default {
 
 <style>
 .fx-chip-group {
+  --style--font: var(--color--font-1);
+  --style--background: transparent;
+  --style--border: transparent;
+  --style--color: var(--color--primary);
+
+  --fx-chip-group--spacing: 5px;
+
+  border: 1px solid var(--style--border);
+  background-color: var(--style--background);
+  padding: calc(var(--fx-chip-group--spacing) * 2);
   overflow: hidden;
   display: flex;
-  flex-direction: row;
+  flex-wrap: wrap;
+  //max-width: 500px;
+
+  &.is-scrollable {
+
+  }
+
+  &.is-left {
+    justify-content: left;
+    .fx-chip { margin-left: 0px }
+  }
+
+  &.is-center {
+    justify-content: center;
+  }
+
+  &.is-right {
+    justify-content: right;
+    .fx-chip { margin-right: 0px }
+  }
 
   .fx-chip {
-    margin-left: 4px;
-
-    &:first-child {
-      margin-left: 0;
-    }
+    //margin: var(--fx-chip-group--spacing);
   }
 
   &.has-overflow {
