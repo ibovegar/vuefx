da097c57bb183a71945fee18d8b4ced7302ff17f
Vegar Eeg <vegareeg@gmail.com>
2018-11-22 16:22:47 +0100

    build: Ignore x-invalid-end-tag

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'brace-style': [2, 'stroustrup', { 'allowSingleLine': true }],
+    'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }],
     'import/no-webpack-loader-syntax': 'off'
   },
   parserOptions: {
