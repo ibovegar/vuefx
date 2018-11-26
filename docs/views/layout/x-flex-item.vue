cf54a98203635f692991f5d9b81d8dd3851b8c06
Vegar Eeg <vegareeg@gmail.com>
2018-11-26 22:43:10 +0100

    docs: Add flexItem example

---
+++ docs/views/layout/x-flex-item.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <Paper height="300px">
+      <Flex :gutter="30" height="100%">
+        <FlexItem self="flex-end">
+          <Paper :height="120" :width="80" fx-background="warning"/>
+        </FlexItem>
+        <FlexItem :width="200" :height="100" self="center">
+          <Paper height="100%" fx-background="warning"/>
+        </FlexItem>
+        <FlexItem flex="1">
+          <Paper :height="80" fx-background="warning"/>
+        </FlexItem>
+      </Flex>
+    </Paper>
+  </section>
+</template>
