69b61f259ab61fc1f1627bbf59dd914c026046a6
Vegar Eeg <vegareeg@gmail.com>
2018-11-26 22:57:24 +0100

    feat: Add width, height, radius and spacing props

--- src/components/Paper/Paper.vue
+++ src/components/Paper/Paper.vue
@@ -1,22 +1,50 @@
-<template functional>
+<template>
   <div
     class="fx-paper"
-    :class="`fx-elevation-${props.elevation}`"
-    :style="{ width: props.width, height: props.height, borderRadius: props.radius }"
-     >
+    :class="[`fx-elevation-${elevation}`, rootClasses]"
+    :style="[rootStyles, colorStyles]"
+    >
     <slot/>
   </div>
 </template>
 
 <script>
+import ColorMixin from '@/mixins/colors.js'
+
 export default {
   name: 'FxPaper',
 
+  mixins: [ ColorMixin ],
+
   props: {
     elevation: { type: Number, default: 1 },
-    width: { type: String, default: null },
-    height: { type: String, default: null },
-    radius: { type: String, default: '3px' }
+    width: { type: [Number, String], default: null },
+    height: { type: [Number, String], default: null },
+    radius: { type: String, default: '3px' },
+    spacing: {
+      type: String,
+      default: null,
+      validator: (value) => {
+        return ['small', 'medium', 'large'].includes(value)
+      }
+    }
+  },
+
+  computed: {
+    rootStyles: function () {
+      return {
+        width: Number.isInteger(this.width) ? this.width + 'px' : this.width,
+        height: Number.isInteger(this.height) ? this.height + 'px' : this.height,
+        borderRadius: this.radius
+      }
+    },
+    rootClasses: function () {
+      return {
+        'spacing--small': this.spacing === 'small',
+        'spacing--medium': this.spacing === 'medium',
+        'spacing--large': this.spacing === 'large'
+      }
+    }
   }
 }
 </script>
@@ -25,10 +53,12 @@ export default {
 .fx-paper {
   --style--font: var(--color--font-1);
   --style--background: var(--color--fill-1);
-  --style--border: var(--color--fill-1);
 
   background-color: var(--style--background);
   color: var(--style--font);
-  border: 1px solid var(--style--border);
+
+  &.spacing--small { padding: var(--spacing--small) }
+  &.spacing--medium { padding: var(--spacing--medium) }
+  &.spacing--large { padding: var(--spacing--large) }
 }
 </style>
